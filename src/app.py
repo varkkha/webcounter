df19f2a992346c1a42c5d8a41960dc0d2da49c7b
Hanna Varkki <h.varkki@gmail.com>
2024-11-13 11:13:50 +0200

    Tehtävä 4 testailua

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 from counter import Counter
 
 app = Flask(__name__)
@@ -12,12 +12,12 @@ def index():
 def increment():
     cnt.increase()
     return redirect("/")
-    
+
 @app.route("/reset", methods=["POST"])
 def reset():
     cnt.reset()
     return redirect("/")
-    
+
 @app.route("/set_value", methods=["POST"])
 def set_value():
     new_value = int(request.form["value"])
